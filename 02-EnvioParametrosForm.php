68cea4eaece03a2f0571fa803cd8e7e95cbe64f5
Programador753 <antoniohcavero@outlook.com>
2024-01-16 09:43:02 +0100

    actualizo

---
+++ 02-EnvioParametrosForm.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="generator" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>02-EnvioParametrosForm.php</title>
+</head>
+<body>
+    <form action="02-RecibeParametrosForm.php" method="post">
+        <table border="1" align="center">
+            <tr>
+                <td>Usuario:</td>
+                <td><input type="text" name="usu" id="usu"></td>
+            </tr>
+            <tr>
+                <td>Contraseña:</td>
+                <td><input type="password" name="cla" id="cla"></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input type="submit" value="ENVIAR"></td>
+            </tr>
+        </table>
+        
+    </form>
+</body>
+</html>
---
+++ 02-RecibeParametrosForm.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="generator" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>02-RecibeParametrosForm.php</title>
+</head>
+<body>
+    <?php
+        echo "El usuario es $_POST[usu] y la contraseña es $_POST[cla] <br>";
+        echo "<a href='02-EnvioParametrosForm.php'>Volver</a>";
+    ?>
+    <br><br>
+    <?php
+        $usuario = $_POST['usu'];
+        $contraseña = $_POST['cla'];
+        echo "El usuario es $usuario y la contraseña es $contraseña <br>";
+        echo "<a href='02-EnvioParametrosForm.php'>Volver</a>";
+    ?>
+</body>
+</html>
