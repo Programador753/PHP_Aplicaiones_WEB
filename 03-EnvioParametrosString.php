4f8113e0878ad3d4798f5dcb9d245cb9b624b9ff
Programador753 <antoniohcavero@outlook.com>
2024-01-16 10:31:07 +0100

    actualizo

---
+++ 03-EnvioParametrosString.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="generator" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>03-EnvioParametrosString.php</title>
+</head>
+<body>
+    <a href="03-RecibeParametrosString.php?usuario=Antonio&curso=2e&clase=AW">Ir a la página RecibeParametrosSrting</a>
+</body>
+</html>
