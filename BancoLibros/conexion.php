77fc588e8ff0311ce4d0546e3fa6ae51cb24cb41
Programador753 <antoniohcavero@outlook.com>
2024-01-24 08:28:34 +0100

    Update conexion.php

--- BancoLibros/conexion.php
+++ BancoLibros/conexion.php
@@ -7,5 +7,7 @@ root -> Usuario de la base de datos
 "" -> Contraseña del usuario de la base de datos
 bancolibros -> Nombre de la base de datos
 
+Mediante este archivo nos conectamos a la base de datos y asi solo tenemos que modificar este archivo si cambiamos de servidor o de usuario de la base de datos o de contraseña o de nombre de la base de datos y no tenemos que modificar todos los archivos que se conectan a la base de datos.
+
 */
 ?>
