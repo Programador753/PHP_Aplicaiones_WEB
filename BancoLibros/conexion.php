0230aa69781d67ebc9226a7cf6fe301b91c4715a
Programador753 <antoniohcavero@outlook.com>
2024-01-24 08:26:50 +0100

    Update conexion.php

--- BancoLibros/conexion.php
+++ BancoLibros/conexion.php
@@ -1,4 +1,11 @@
 <?php
 // conectamos con la base de datos
 $conexion=mysqli_connect("localhost","root","","bancolibros") or die("Problemas en la conexion");
+/*
+localhost _> Servidor al que nos conectamos
+root -> Usuario de la base de datos
+"" -> Contraseña del usuario de la base de datos
+bancolibros -> Nombre de la base de datos
+
+*/
 ?>
