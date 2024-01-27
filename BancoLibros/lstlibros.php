cc250895014b32648bb8932f5b5854967f304cba
Programador753 <antoniohcavero@outlook.com>
2024-01-27 12:49:04 +0100

    Create lstlibros.php

---
+++ BancoLibros/lstlibros.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="generator" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>formLibros.php</title>
+</head>
+
+<body>
+    <?php
+    // conectamos con la base de datos
+    include("conexion.php");
+    
+    ?>
+</body>
+</html>
